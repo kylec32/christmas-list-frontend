47307ed45e07d130d1f75c97d5c548daf72f777e
Kyle Carter <kylec32@gmail.com>
2018-09-23 07:55:28 -0600

    Removed comment

--- src/app/services/base-http.service.ts
+++ src/app/services/base-http.service.ts
@@ -3,6 +3,5 @@ import { RequestOptions, Headers } from '@angular/http';
 
 @Injectable()
 export class BaseHttpService {
-    //BASE_URL = "http://localhost:3000";
     BASE_URL:string = "https://9jok0yonc3.execute-api.us-west-2.amazonaws.com/dev";
 }    
