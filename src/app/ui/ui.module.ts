be0a58f85584c6d79cf197ef892923fa2fb7510c
Kyle Carter <kylec32@gmail.com>
2020-11-05 21:40:22 -0700

    Pre-update stuff

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule, MatButtonModule, MatInputModule, MatSnackBarModule, MatListModule, MatDialogModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   imports: [
